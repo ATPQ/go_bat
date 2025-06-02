52503266489bf186757d888865183a2b6b25c968
ATPQ <48762829+ATPQ@users.noreply.github.com>
2025-06-02 23:09:47 +0800

    Update 应用投屏.bat
    bug 待修复

--- bat/应用投屏.bat
+++ bat/应用投屏.bat
@@ -16,9 +16,7 @@ if %device_connected% equ 1 (
     echo.
     choice /c 12 /n /m "请选择：[1]使用已有设备 [2]输入新IP连接"
     if !errorlevel! equ 1 (
-        echo 正在启动 scrcpy...
-        .\scrcpy --new-display --start-app=com.dragon.read
-        goto exit_adb
+        goto menu
     )
 )
 
@@ -47,13 +45,70 @@ echo 正在连接 ADB: %full_ip%
 .\adb connect %full_ip%
 .\adb devices | findstr "%full_ip%" >nul
 if %errorlevel% neq 0 (
-    echo 错误：ADB连接失败
-    pause
-    exit /b 1
+    echo 错误：ADB连接失败，尝试验证码登录
+    echo.
+    choice /c 12 /n /m "请选择：[1]输入验证码连接 [2]输入新端口并验证连接"
+    if !errorlevel! equ 1 (
+        .\adb pair %full_ip%
+    ) else if !errorlevel! equ 2 (
+		set /p "user_input=请输入新的端口号 ："
+		:: 直接用字符串替换，找到冒号后面的内容并替换
+		for /f "tokens=1 delims=:" %%a in ("%full_ip%") do (
+			set soue_ip=%%a
+		)
+		set full_ip = %soue_ip%:%user_input%
+		.\adb pair %full_ip%
+		if %errorlevel% neq 0 (
+			goto connect_devices
+		)
+	)
 )
 
-echo 正在启动 scrcpy...
-.\scrcpy --new-display --start-app=com.dragon.read
+
+
+
+:menu
+cls
+echo ============================
+echo 请选择要启动的应用：
+echo 0. 退出
+echo 1. 英语软件
+echo 2. 番茄小说
+echo 3. 其他应用
+echo ============================
+set /p choice=请输入数字并按回车:
+
+:: 定义基础的scrcpy命令
+set SCRCPY_CMD=.\scrcpy --new-display --start-app=
+
+if "%choice%"=="0" (
+	echo 退出程序。
+    goto end
+) else if "%choice%"=="1" (
+    echo 正在启动英语软件...
+    %SCRCPY_CMD%com.shanbay.sentence
+    goto end
+) else if "%choice%"=="2" (
+	echo 正在启动番茄小说...
+    %SCRCPY_CMD%com.dragon.read
+    goto end
+    
+) else if "%choice%"=="3" (
+    echo 请输入应用的包名：
+    set /p appname=应用包名：
+    echo 正在启动自定义应用...
+    %SCRCPY_CMD%%appname%
+    goto end
+) else (
+    echo 输入无效，请重新选择。
+    pause
+    goto menu
+)
+
+:end
+
+::echo 正在启动 scrcpy...
+::.\scrcpy --new-display --start-app=com.dragon.read
 
 :: 清理
 :exit_adb
